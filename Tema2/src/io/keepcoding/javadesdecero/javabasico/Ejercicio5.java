71d440219ae1f5b7fa6b49fc3885b52f12a9e0e8
Rubén Fresneda <cem20814@gmail.com>
2024-05-28 22:11:22 +0200

    no me entero de un carajo

---
+++ Tema2/src/io/keepcoding/javadesdecero/javabasico/Ejercicio5.java
@@ -0,0 +1,15 @@
+package io.keepcoding.javadesdecero.javabasico;
+
+public class Ejercicio5 {
+
+	public static void main(String[] args) {
+		double a = 5;
+		double b = 3;
+		double c = 7;
+		System.out.println("El área de " + a + ", " + b + " y " + c)
+
+	}
+	public static void area(int radius) {
+		
+	}
+}
